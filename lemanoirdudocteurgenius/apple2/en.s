4a8d910389f58fbc2c6ef9babcdd59b5b7f34df3
Antoine Vignau <34219772+antoinevignau@users.noreply.github.com>
2023-11-27 11:06:46 +0100

    In English in the text

--- lemanoirdudocteurgenius/apple2/en.s
+++ lemanoirdudocteurgenius/apple2/en.s
@@ -27,17 +27,17 @@ strEVIDENT
 	asc	8D"Il parait evident que vous ne pouvez"8D
 	asc	"pas porter tant de choses !!"00
 strVOUSLAVEZ
-	asc	8D"Vous l"A7"avez deja. Voues etes etourdi"8D
-	asc	"et dans cette maison, ce n"A7"est pas"00
+	asc	8D"Vous l'avez deja. Voues etes etourdi"8D
+	asc	"et dans cette maison, ce n'est pas"00
 strCONSEILLE
 	asc	8D"tres conseille"00
 	
 strNOTOWNED
 	asc	8D"Comment voulez-vous poser ce que vous"8D
-	asc	"n"A7"avez pas ?"00
+	asc	"n'avez pas ?"00
 
 strDACCORD
-	asc	8D"D"A7"accord"00
+	asc	8D"D'accord"00
 	
 *-----------------------------------
 * 4000 - LES REPONSES
@@ -47,15 +47,15 @@ str4000	asc	"VOUS AVEZ GARDE LA LAMPE TROP LONGTEMPS"8D
 	asc	"ALLUMEE, ELLE A EXPLOSE"00
 
 str4010	asc	"VOUS AVEZ OUBLIE DE FERMER LE ROBINET"8D
-	asc	"VOUS MOUREZ SOUS DES TONNES D"A7"EAU"00
+	asc	"VOUS MOUREZ SOUS DES TONNES D'EAU"00
 
 str4020	asc	"LA PORTE VIENT DE SE REFERMER DERRIERE"8D
 	asc	"VOUS, VOUS VOILA PRISONNIER..."00
 
-str4030	asc	"VOUS AVEZ TREBUCHE DANS L"A7"ESCALIER, VOUS"
+str4030	asc	"VOUS AVEZ TREBUCHE DANS L'ESCALIER, VOUS"
 	asc	"VOUS EMPALEZ SUR LE COUTEAU !"00
 
-str4040	asc	"VOUS RENVERSEZ L"A7"EAU DANS L"A7"ESCALIER,"8D
+str4040	asc	"VOUS RENVERSEZ L'EAU DANS L'ESCALIER,"8D
 	asc	"CE QUI PROVOQUE UNE DECHARGE DE LA"00
 str4042	asc	8D"PRISE ELECTRIQUE"00
 
@@ -72,7 +72,7 @@ str4072	asc	8D"ON RAMASSERA VOS MORCEAUX UN AUTRE"8D
 str4080	asc	"VOUS MOUREZ EMPALE SUR DES LANCES"8D
 	asc	"SORTIES DU MUR... !"00
 
-str4090	asc	"LA PORTE NE S"A7"OUVRE PAS DE CETTE PIECE"00
+str4090	asc	"LA PORTE NE S'OUVRE PAS DE CETTE PIECE"00
 
 str4100	asc	"LA LAMPE ET LE BRIQUET REFUSENT DE"8D
 	asc	"MARCHER DANS CETTE PIECE"00
@@ -81,19 +81,19 @@ str4110	asc	"VOUS TOMBEZ DANS UNE TRAPPE, VOUS VOUS"8D
 	asc	"DISLOQUEZ EN ARRIVANT AU SOL..."00
 
 str4120	asc	"VOUS AVEZ RAISON DE PASSER, CAR CE"8D
-	asc	"MONSTRE N"A7"ETAIT QU"A7"UNE PROJECTION"00
+	asc	"MONSTRE N'ETAIT QU'UNE PROJECTION"00
 str4124	asc	8D"EN 3 DIMENSIONS SUR UN ECRAN DE FUMEE"00
 
 str4130	asc	"VOUS AVEZ RAISON, LA CURIOSITE EST UN"8D
 	asc	"VILAIN DEFAUT"00
 str4133	asc	8D"            AU REVOIR"00
 
-str4140	asc	"VOUS AVEZ RAISON D"A7"ATTENDRE, MAIS CELA"
+str4140	asc	"VOUS AVEZ RAISON D'ATTENDRE, MAIS CELA"
 	asc	"NE POURRA PAS DURER ETERNELLEMENT..."00
 
 str4150	asc	"VOUS AVEZ DE LA CHANCE CAR CE COFFRE"8D
 	asc	"ETAIT OUVERT."00
-str4152	asc	8D"UN MESSAGE A L"A7"INTERIEUR DIT : NE"8D
+str4152	asc	8D"UN MESSAGE A L'INTERIEUR DIT : NE"8D
 	asc	"RESPECTEZ PAS LES COULEURS DU CODE DE LA"8D
 	asc	"ROUTE...?"00
 str4156	asc	8D"TIENS LE COFFRE SE REFERME"00
@@ -101,52 +101,52 @@ str4156	asc	8D"TIENS LE COFFRE SE REFERME"00
 str4160	asc	"MAINTENANT, VOUS AVEZ UNE LAMPE PLEINE"8D
 	asc	"DE PETROLE"00
 
-str4170	asc	"VOUS N"A7"AVEZ RIEN POUR TRANSPORTER LE"8D
+str4170	asc	"VOUS N'AVEZ RIEN POUR TRANSPORTER LE"8D
 	asc	"PETROLE"00
 
 str4180	asc	"LE BRIQUET QUE VOUS AVIEZ LAISSE ALLUME"8D
-	asc	"VIENT D"A7"EXPLOSER"00
-str4185	asc	8D"CA TUE L"A7"ETOURDIE..."00
+	asc	"VIENT D'EXPLOSER"00
+str4185	asc	8D"CA TUE L'ETOURDIE..."00
 
 str4190	asc	"A FORCE DE MARCHER EN LONG ET EN LARGE"8D
 	asc	"dans CETTE MAISON,"00
 str4195	asc	8D"VOUS SOMBREZ DANS UN COMA DES PLUS"8D
 	asc	"MORTELS..."00
 
-str4200	asc	"L"A7"EAU COULE..."00
+str4200	asc	"L'EAU COULE..."00
 
 str4210	asc	"VOUS AVEZ LES PIEDS TREMPES ET CELA VOUS"
 	asc	"REND TRES MALADE..."00
-str4215	asc	8D"VOUS MOUREZ D"A7"UNE TRIPLE PNEUMONIE...!"00
+str4215	asc	8D"VOUS MOUREZ D'UNE TRIPLE PNEUMONIE...!"00
 
 str4220	asc	"LE TITRE EST : "00
 str4225	asc	8D"LA MORT A LA PREMIERE PAGE."00
 
-str4230	asc	"LE LIVRE A EXPLOSE LORSQUE VOUS L"A7"AVEZ"8D
+str4230	asc	"LE LIVRE A EXPLOSE LORSQUE VOUS L'AVEZ"8D
 	asc	"OUVERT..."00
 
 str4240	asc	"LE PAPIER INDIQUE : CHERCHEZ LA CLEF."00
 
 str4250	asc	"LA CLEF VOUS PERMETTRA DE TROUVER LE"8D
-	asc	"CODE DE LA PORTE D"A7"ENTREE."00
+	asc	"CODE DE LA PORTE D'ENTREE."00
 
 str4260	asc	"IL Y A, A COTE DE LA PORTE, UN CLAVIER"8D
-	asc	"NUMERIQUE PERMETTANT D"A7"ENTRER UN CODE"00
+	asc	"NUMERIQUE PERMETTANT D'ENTRER UN CODE"00
 
 str4270	asc	"POUR FAIRE QUOI...?"00
 
 str4280	asc	8D"IL Y A UNE ODEUR DE GAZ."00
 
-str4290	asc	"APPAREMMENT, IL N"A7"Y A AUCUNE ODEUR"8D
+str4290	asc	"APPAREMMENT, IL N'Y A AUCUNE ODEUR"8D
 	asc	"MAIS..."00
 
-str4300	asc	"C"A7"EST DEJA FAIT, ESPECE DE RIGOLO"00
+str4300	asc	"C'EST DEJA FAIT, ESPECE DE RIGOLO"00
 
 str4310	asc	"IL FAUDRAIT PEUT-ETRE DU FEU"00
 
 str4320	asc	"LA LAMPE NE CONTIENT PAS DE PETROLE"00
 
-str4330	asc	"VOUS NE L"A7"AVEZ PAS"00
+str4330	asc	"VOUS NE L'AVEZ PAS"00
 
 str4340	asc	"LE BRIQUET EST ENCORE ALLUME ET IL"8D
 	asc	"ECLAIRE LA PIECE."00
@@ -161,19 +161,19 @@ str4370	asc	"UN NAIN VIENT DE VOUS LANCER UN POIGNARD"
 	asc	"EN PLEIN COEUR..."00
 	
 str4380	asc	"UN NAIN VIENT DE SE PRECIPITER SUR VOUS,"
-	asc	"IL S"A7"EMPALE SUR VOTRE CISEAU"00
+	asc	"IL S'EMPALE SUR VOTRE CISEAU"00
 
 str4390	asc	"UN NAIN VIENT DE SE PRECIPITER SUR VOUS,"
-	asc	"IL S"A7"EMPALE SUR VOTRE COUTEAU"00
+	asc	"IL S'EMPALE SUR VOTRE COUTEAU"00
 
 str4400	asc	"VOUS VENEZ DE RENVERSER LE POT"00
 
 str4410	asc	"LA FOUDRE VIENT DE TOMBER SUR LA MAISON"8D
-str4412	asc	8D"LA MAISON N"A7"EXISTE PLUS, VOUS NON PLUS"00
+str4412	asc	8D"LA MAISON N'EXISTE PLUS, VOUS NON PLUS"00
 
 str4420	asc	"A FORCE DE MARCHER DANS LE NOIR, VOUS"8D
 	asc	"AVEZ TREBUCHE"00
-str4425	asc	8D"VOUS MOUREZ D"A7"UNE FRACTURE DU CRANE"00
+str4425	asc	8D"VOUS MOUREZ D'UNE FRACTURE DU CRANE"00
 
 str4430	asc	"VOUS NE POUVEZ PAS TRAVAILLER DANS LE"8D
 	asc	"NOIR..."00
@@ -183,12 +183,12 @@ str4440	asc	"LA LUMIERE DU BRIQUE NE SUFFIT PAS"8D
 
 str4450	asc	"IMPOSSIBLE !"8D00
 
-str4460	asc	"VOUS N"A7"AVEZ AUCUN OUTIL..."
+str4460	asc	"VOUS N'AVEZ AUCUN OUTIL..."
 
 str4470	asc	"LE TELEPORTEUR EST EN PANNE, LES BOUTONS"
 	asc	"NE FONCTIONNENT PAS."00
 
-str4480	asc	"LE TELEPORTEUR VIENT D"A7"EXPLOSER, VOUS"8D
+str4480	asc	"LE TELEPORTEUR VIENT D'EXPLOSER, VOUS"8D
 	asc	"ETES DECOMPOSEE... !"00
 	
 str4490	asc	"LE TELEPORTEUR SE MET EN MARCHE, VOUS"8D
@@ -199,15 +199,15 @@ str4500	asc	"VOUS PRENEZ DU 30000 VOLTS DANS LES"8D
 
 str4510	asc	"LE PLACARD EST FERME A CLEF"00
 
-str4520	asc	"L"A7"HORRIBLE MONSTRE SORTI DU PLACARD"8D
+str4520	asc	"L'HORRIBLE MONSTRE SORTI DU PLACARD"8D
 	asc	"VIENT DE VOUS DEVORER"00
 
 str4530	asc	"IL NE FALLAIT PAS FUIR"00
 
-str4540	asc	"VOUS AVEZ RAISON D"A7"UTILISER LE CISEAU,"8D
+str4540	asc	"VOUS AVEZ RAISON D'UTILISER LE CISEAU,"8D
 	asc	"LE MONSTRE EST MORT"00
 
-str4550	asc	"A L"A7"INTERIEUR DU PLACARD, LE NO "00
+str4550	asc	"A L'INTERIEUR DU PLACARD, LE NO "00
 str4552	asc	8D" EST INSCRIT"00
 str4555	asc	8D"LE PLACARD SE REFERME."00
 
@@ -218,27 +218,27 @@ str4570	asc	"LE CLAVIER NUMERIQUE A EXPLOSE"00
 str4580	asc	"LE CLAVIER NUMERIQUE PREND FEU,"8D
 	asc	"HEUREUSEMENT, VOUS AVIEZ "00
 str4582	asc	"UN POT PLEIN"00
-str4585	asc	8D"D"A7"EAU QUI VOUS PERMET D"A7"ETEINDRE LE FEU"00
+str4585	asc	8D"D'EAU QUI VOUS PERMET D'ETEINDRE LE FEU"00
 
 str4590	asc	8D"NO DE CODE? "00
 
 strCODEEXACT
-	asc	"LE CODE EST EXACT... LA PORTE S"A7"OUVRE..."00
+	asc	"LE CODE EST EXACT... LA PORTE S'OUVRE..."00
 strENDEHORS
 	asc	8D"VOUS VOILA EN DEHORS DE LA MAISON..."
 
-str4610	asc	"A L"A7"INTERIEUR DU PLACARD, IL Y A UN MOT"8D
-	asc	"PARLE D"A7"UN TELEPORTEUR"00
+str4610	asc	"A L'INTERIEUR DU PLACARD, IL Y A UN MOT"8D
+	asc	"PARLE D'UN TELEPORTEUR"00
 str4615	asc	8D"TIENS LE PLACARD SE FERME TOUT SEUL..."00
 
 str4620	asc	"AVANT DE LA POSER A TERRE, IL FAUDRAIT"8D
-	asc	"PEUT-ETRE L"A7"ENLEVER"00
+	asc	"PEUT-ETRE L'ENLEVER"00
 
 str4630	asc	"IL Y A UN HORRIBLE MONSTRE DEVANT VOUS"8D
 	asc	"QUI EST SORTI DU PLACARD."00
 
-str4640	asc	"LE PLACARD ETAIT PIEGE, VOUS N"A7"AURIEZ"8D
-	asc	"PAS DU L"A7"OUVRIR"00
+str4640	asc	"LE PLACARD ETAIT PIEGE, VOUS N'AURIEZ"8D
+	asc	"PAS DU L'OUVRIR"00
 
 *-----------------------------------
 * LIEUX
@@ -248,82 +248,83 @@ str4640	asc	"LE PLACARD ETAIT PIEGE, VOUS N"A7"AURIEZ"8D
 *		"0123456789012345678901234567890123456789"
 *		"----------------------------------------"
 
-strVOUS	asc	8D"Vous etes "00
-str7000	asc	"devant le manoir du defunt"00
+strVOUS	asc	8D"You are "00
+str7000	asc	"in front of the manor of"00
 str7001	asc	8D"            Dr Genius"00
-str7010	asc	"dans le hall d"A7"entree"00
-str7020	asc	"EN BAS DE L"A7"ESCALIER MENANT AU2E ETAGE"00
-str7030	asc	"dans LA SALLE A MANGER"00
-str7040	asc	"dans UNE BIBLIOTHEQUE SANS LIVRE...!"00
-str7050	asc	"dans UNE BUANDERIE"00
-str7060	asc	"dans LE SALON"00
-str7070	asc	"dans UNE CHAMBRE"00
-str7080	asc	"dans UN CORRIDOR"00
-str7090	asc	"dans UNE SALLE D"A7"ATTENTE"00
-str7100	asc	"dans LE VESTIBULE"00
-str7110	asc	"dans LA CHAMBRE D"A7"AMIS"00
-str7120	asc	"dans UNE CHAMBRE"00
+str7010	asc	"in the entrance hall"00
+str7020	asc	"At the bottom of the stairs tothe 2nd floor"00
+str7030	asc	"in the dining room"00
+str7040	asc	"in a library without books...!"00
+str7050	asc	"in a laundry room"00
+str7060	asc	"in the living room"00
+str7070	asc	"in a bedroom"00
+str7080	asc	"in a corridor"00
+str7090	asc	"in a waiting room"00
+str7100	asc	"in a vestibule"00
+str7110	asc	"in the guest room"00
+str7120	asc	"in a bedroom"00
 str7130	asc	""00	; nada
-str7140	asc	"dans UNE PETITE SALLE"00
-str7150	asc	"dans LE LABORATOIRE DU"00	; + :7001
-str7160	asc	"dans UNE PETITE PIECE VIDE"00
-str7170	asc	"! JUSTEMENT, VOUS NE SAVEZ PASOU VOUS ETES"00
-str7180	asc	"EN HAUT DE L"A7"ESCALIER"00
-str7190	asc	"dans LA SALLE DE BAINS"00
-str7200	asc	"dans LE LIVING ROOM"00
-str7210	asc	"dans UNE PIECE ENFUMEE"00
-str7220	asc	"dans UNE GRANDE PIECE"00
-str7230	asc	"dans UNE PIECE DE RANGEMENT"00
-str7240	asc	"dans LE DRESSING"00
+str7140	asc	"in a small room"00
+str7150	asc	"in the laboratory of"00	; + :7001
+str7160	asc	"in a small empty room"00
+str7170	asc	"! You actually do not know"8D"
+	asc	"where you are"00
+str7180	asc	"at the top of the stairs"00
+str7190	asc	"in the bathroom"00
+str7200	asc	"in the living room"00
+str7210	asc	"in a somky room"00
+str7220	asc	"in a large room"00
+str7230	asc	"in a storage room"00
+str7240	asc	"in the dressing room"00
 
-strREPLAY	asc	8D"Voulez-vous rejouer ? "00
+strREPLAY	asc	8D"Do you want to play again? "00
+
+*		"0123456789012345678901234567890123456789"
 	
-strGAGNE	asc	"CELA EST EXCEPTIONNEL. VOUS ETES LE"8D8D
-	asc	"PREMIER A ETRE SORTI VIVANT DE CETTE"8D8D
-	asc	"MAISON, MAIS SI J"A7"ETAIS VOUS, JE ME"8D8D
-	asc	"METTRAIS A COURIR CAR UN NAIN RODE"8D8D
-	asc	"PEUT-ETRE DANS LES PARAGES..."00
+strGAGNE	asc	"This is exceptional, you are the first"8D8D
+	asc	"to get out of this house alive, but"8D8D
+	asc	"if I were you, I would start to flee"8D8D
+	asc	"because a dwarf may be lurking around..."00
 	
 *-----------------------------------
 * 40000 - LISTE DES INSTRUCTIONS
 *-----------------------------------
 
-strINSTR	asc	8D"La liste des instructions ? "00
+strINSTR	asc	8D"Do you want to know how to play? "00
 
 strINSTR2	asc	8D8D
-	asc	"Vous voici arrive dans le manoir du"8D
+	asc	"You have arrived in the mansion of"8D
 	asc	"            Dr Genius..."8D
 	asc	8D
-	asc	"Pour converser avec l"A7"ordinateur, il"8D
-	asc	"faut rentrer les ordres en 1 ou 2 mots"8D
-	asc	"tels que :"8D
-	asc	"           NORD"8D
-	asc	"           PRENDS PILULE"8D
+	asc	"To converse with the computer, you"8D
+	asc	"must enter orders in 1 or 2 words"8D
+	asc	"such as"8D
+	asc	"           NORTH"8D
+	asc	"           TAKE PILL"8D
 	asc	8D
-	asc	"ou pour commencer :"8D
-	asc	"           ENTRE"8D
+	asc	"or to start:"8D
+	asc	"           ENTER"8D
 	asc	8D8D
-	asc	"Si vous voulez faire durer la phrase"8D
-	asc	"décrivant la salle, tapez une touche"8D
+	asc	"If you want to make the sentence"8D
+	asc	"describing the room last, type a key"8D
 	asc	8D
-	asc	"Un dernier conseil : il peut parfois y"8D
-	asc	"avoir une porte derriere vous. "00
+	asc	"One last advice: sometimes, there may"8D
+	asc	"be a door behind you. "00
 
 *-----------------------------------
 * 51000 - DISCLAIMER
 *-----------------------------------
 
 strDISCLAIMER
-	asc	"L"A7"UTLISATION DE CE PROGRAMME EST"8D8D
-	asc	"DECONSEILLEE AUX PERSONNES SENSIBLES,"8D8D
-	asc	"AUX ENFANTS EN BAS AGE, AINSI QU"A7"A"8D8D
-	asc	"TOUTE PERSONNE SUSCEPTIBLE D"A7"AVOIR"8D8D
-	asc	"DES MALAISES CARDIAQUES."8D8D
+	asc	"The use of this program is not"8D8D
+	asc	"recommended to sensitive people,"8D8D
+	asc	"young children, as well as"8D8D
+	asc	"anyone like to have heart diseases."8D8D
 	asc	8D8D
-	asc	"NOUS NE POURRIONS ETRE TENUS RESPONSA-"8D8D
-	asc	"-BLES DES TROUBLES PHYSIQUES OU MENTAUX"8D8D
-	asc	"PROVOQUES PAR VOTRE ECHEC DANS"8D8D
-	asc	"LE MANOIR DU DR GENIUS ............."00
+	asc	"We cannot be held responsible for any"8D8D
+	asc	"physical or mental disorder caused"8D8D
+	asc	"by your failure in"8D8D
+	asc	"The Manor of Dr Genius ............."00
 	
 *-----------------------------------
 * introPIC - la picture GR
@@ -374,121 +375,121 @@ V$9	str	"UP"	; MONTER
 V$10	str	"CLIM"	; MONTER
 V$11	str	"DOWN"	; DESCENDRE
 V$12	str	"TAKE"	; PRENDRE
-V$13	str	"RAMA"	; RAMASSER
-V$14	str	"POSE"	; POSER
+V$13	str	"PICK"	; RAMASSER
+V$14	str	"DROP"	; POSER
 V$15	str	"OPEN"	; OUVRIR
 V$16	str	"CLOS"	; FERMER
 V$17	str	"ENTE"	; ENTRER
 V$18	str	"MOVE"	; AVANCER
 V$19	str	"LIGH"	; ALLUMER
-V$20	str	"ETEI"	; ETEINDRE
+V$20	str	"EXTI"	; ETEINDRE
 V$21	str	"REPA"	; REPARER
 V$22	str	"DEPA"	; DEPANNER
-V$23	str	"LIS"	; LIS
-V$24	str	"REGA"	; REGARDER
-V$25	str	"RETO"	; RETOURNER
-V$26	str	"RENI"	; RENIFLER
-V$27	str	"SENS"	; SENS ?
-V$28	str	"REMP"	; REMPLIT
-V$29	str	"VIDE"
-V$30	str	"INVE"
-V$31	str	"LIST"
-V$32	str	"RIEN"
-V$33	str	"ATTE"
-V$34	str	"POIG"
-V$35	str	"COUT"
-V$36	str	"TOUR"
-V$37	str	"LAMP"
-V$38	str	"CODE"
-V$39	str	"ESCA"
-V$40	str	"PIST"
-V$41	str	"PLAC"
-V$42	str	"TORC"
-V$43	str	"TELE"
-V$44	str	"MONS"
-V$45	str	"PETR"
-V$46	str	"POT"
-V$47	str	"LIT"
-V$48	str	"CLEF"
-V$49	str	"PAPI"
-V$50	str	"LIVR"
-V$51	str	"BRIQ"
-V$52	str	"COMB"
-V$53	str	"COFF"
-V$54	str	"ROUG"
-V$55	str	"BLEU"
-V$56	str	"VERT"
-V$57	str	"TITR"
-V$58	str	"ROBI"
-V$59	str	"CISE"
-V$60	str	"PORT"
-V$61	str	"ACTI"
-V$62	str	"JETE"
-V$63	str	"LANCE"
-V$64	str	"EAU"
-V$65	str	"ENFI"
-V$66	str	"PASS"
-V$67	str	"APPU"
-V$68	str	"ENFO"
-V$69	str	"ENLE"
-V$70	str	"RENT"
-V$71	str	"TEMPO"	; Apple II
-V$72	str	"QUITTER"	; Apple II
-V$73	str	"CASSE"	; Apple II
+V$23	str	"READ"	; LIS (LIRE)
+V$24	str	"LOOK"	; REGARDER
+V$25	str	"TURN"	; RETOURNER
+V$26	str	"SNIF"	; RENIFLER
+V$27	str	"SMEL"	; SENS (SENTIR)
+V$28	str	"FILL"	; REMPLIT
+V$29	str	"EMPT"	; VIDER
+V$30	str	"INVE"	; INVENTAIRE
+V$31	str	"LIST"	; LISTE
+V$32	str	"NOTH"	; RIEN
+V$33	str	"WARN"	; ATTENTION
+V$34	str	"DAGG"	; POIGNARD
+V$35	str	"KNIF"	; COUTEAU
+V$36	str	"SCRE"	; TOURNEVIE
+V$37	str	"LAMP"	; LAMPE
+V$38	str	"CODE"	; CODE
+V$39	str	"STAI"	; ESCALIER
+V$40	str	"PIST"	; PISTOLET
+V$41	str	"CLOS"	; PLACARD
+V$42	str	"TORC"	; TORCHE
+V$43	str	"TELE"	; TELEPORTEUR
+V$44	str	"MONS"	; MONSTRE
+V$45	str	"OIL"	; PETROLE
+V$46	str	"POT"	; POT
+V$47	str	"BED"	; LIT
+V$48	str	"KEY"	; CLEF
+V$49	str	"PAPE"	; PAPIER
+V$50	str	"BOOK"	; LIVRE
+V$51	str	"LIGH"	; BRIQUET
+V$52	str	"SUIT"	; COMBINAISON
+V$53	str	"CHES"	; COFFRE
+V$54	str	"RED"	; ROUGE
+V$55	str	"BLUE"	; BLEU
+V$56	str	"GREE"	; VERT
+V$57	str	"TITL"	; TITRE
+V$58	str	"TAP"	; ROBINET
+V$59	str	"SCIS"	; CISEAU
+V$60	str	"CARR"	; PORTER
+V$61	str	"ENAB"	; ACTIVER
+V$62	str	"THRO"	; JETER
+V$63	str	"THRO"	; LANCER
+V$64	str	"WAT"	; EAU
+V$65	str	"WEAR"	; ENFILER
+V$66	str	"PASS"	; PASSER
+V$67	str	"PRES"	; APPUYER
+V$68	str	"BURY"	; ENFOUIR
+V$69	str	"REMO"	; ENLEVER
+V$70	str	"RETU"	; RENTRER
+V$71	str	"TEMPO"	; TEMPO - Apple II
+V$72	str	"QUIT"	; QUITTER - Apple II
+V$73	str	"CASE"	; CASSE - Apple II
 
 *-----------------------------------
 * OBJETS
 *-----------------------------------
 
-O$1	asc	"une torche electrique"00
-O$2	asc	"un robinet"00
-O$3	asc	"un ciseau"00
-O$4	asc	"un tournevis"00
-O$5	asc	"une lampe a petrole"00
-O$6	asc	"une lampe pleine"00
-O$7	asc	"une lampe allumee"00
-O$8	asc	"un couteau"00
-O$9	asc	"un papier"00
-O$10	asc	"un livre"00
-O$11	asc	"du petrole dans un lavabo bouche"00
-O$12	asc	"une clef"00
-O$13	asc	"un bouton rouge"00
-O$14	asc	"un bouton bleu"00
-O$15	asc	"un bouton vert"00
-O$16	asc	"un teleporteur"00
-O$17	asc	"un teleporteur repare"00
-O$18	asc	"une combinaison argentee"00
-O$19	asc	"une combinaison enfilee"00
-O$20	asc	"un monstre a l"A7"est"00
-O$21	asc	"un pistolet"00
-O$22	asc	"un briquet"00
-O$23	asc	"un briquet allume"00
-O$24	asc	"un pot"00
-O$25	asc	"un pot plein d"A7"eau"00
+O$1	asc	"an electric torch"00
+O$2	asc	"a tap"00
+O$3	asc	"scissors"00
+O$4	asc	"a screwdriver"00
+O$5	asc	"an oil lamp"00
+O$6	asc	"a full lamp"00
+O$7	asc	"a lighted lamp"00
+O$8	asc	"a knife"00
+O$9	asc	"a paper"00
+O$10	asc	"a book"00
+O$11	asc	"oil in a clogged sink"00
+O$12	asc	"a key"00
+O$13	asc	"a red button"00
+O$14	asc	"a blue button"00
+O$15	asc	"a green button"00
+O$16	asc	"a teleporter"00
+O$17	asc	"a repaired teleporter"00
+O$18	asc	"a silver suit"00
+O$19	asc	"a coated suit"00
+O$20	asc	"a monster to the east"00
+O$21	asc	"a gun"00
+O$22	asc	"a lighter"00
+O$23	asc	"a lighted lighter"00
+O$24	asc	"a pot"00
+O$25	asc	"a pot full of water"00
 
 *-----------------------------------
 * STRINGS
 *-----------------------------------
 
 strILFAITNOIR
-	asc	"Il fait noir comme dans un four, il"8D
-	asc	"faudrait peut-etre allumer"00
+	asc	"It is very dark, perhaps you should"8D
+	asc	"turn on the lights"00
 
-strILYA	asc	8D"Il y a dans la salle :"00
+strILYA	asc	8D"The room contains:"00
 strCOMMA	asc	","00
 strSPACE	asc	" "00
 strRETURN	asc	8D00
 
 strCOMMANDE
-	asc	8D"Que faites-vous ? "00
+	asc	8D"Command? "00
 
 strJENECOMPRENDS
-	asc	8D"Je ne comprends pas..."00
+	asc	8D"I do not understand..."00
 	
 strIMPOSSIBLE
-	asc	8D"Impossible "00
+	asc	8D"Not possible "00
 strCECHEMIN
-	asc	"de prendre ce chemin"00
+	asc	"to take this path"00
 strEXCLAM
 	asc	" !"00
 	

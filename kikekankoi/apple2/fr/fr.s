dd77a0b6a93520ba76a89cfa2dfb990cc65627b7
Antoine Vignau <34219772+antoinevignau@users.noreply.github.com>
2023-12-11 22:07:02 +0100

    moving on

---
+++ kikekankoi/apple2/fr/fr.s
@@ -0,0 +1,446 @@
+*
+* Le vocabulaire
+*
+
+L7C10       ASC   'N   '
+            HEX   01
+            ASC   'NORD'
+            HEX   01
+            ASC   'S   '
+            HEX   02
+            ASC   'SUD '
+            HEX   02
+            ASC   'E   '
+            HEX   03
+            ASC   'EST '
+            HEX   03
+            ASC   'O   '
+            HEX   04
+            ASC   'OUES'
+            HEX   04
+            ASC   'M   '
+            HEX   05
+            ASC   'MONT'
+            HEX   05
+            ASC   'GRIM'
+            HEX   05
+            ASC   'DESC'
+            HEX   06
+            ASC   'D   '
+            HEX   06
+            ASC   'ENTR'
+            HEX   07
+            ASC   'AVAN'
+            HEX   08
+            ASC   'PREN'
+            HEX   0A
+            ASC   'RAMA'
+            HEX   0A
+            ASC   'POSE'
+            HEX   0B
+            ASC   'LAIS'
+            HEX   0B
+            ASC   'OUVR'
+            HEX   0C
+            ASC   'FERM'
+            HEX   0D
+            ASC   'ALLU'
+            HEX   0E
+            ASC   'ECLA'
+            HEX   0E
+            ASC   'ETEI'
+            HEX   0F
+            ASC   'ARRE'
+            HEX   0F
+            ASC   'LIS '
+            HEX   10
+            ASC   'LIT '
+            HEX   10
+            ASC   'REMP'
+            HEX   12
+            ASC   'VIDE'
+            HEX   13
+            ASC   'INVE'
+            HEX   14
+            ASC   'RIEN'
+            HEX   15
+            ASC   'ATTE'
+            HEX   15
+            ASC   'FRAP'
+            HEX   16
+            ASC   'ASSO'
+            HEX   16
+            ASC   'ATTA'
+            HEX   16
+            ASC   'POUS'
+            HEX   17
+            ASC   'TIRE'
+            HEX   17
+            ASC   'JETT'
+            HEX   18
+            ASC   'MANG'
+            HEX   19
+            ASC   'GOUT'
+            HEX   19
+            ASC   'BOIS'
+            HEX   1A
+            ASC   'LANC'
+            HEX   18
+            ASC   'BARQ'
+            HEX   1B
+            ASC   'RADE'
+            HEX   1C
+            ASC   'BOUT'
+            HEX   1D
+            ASC   'MESS'
+            HEX   1E
+            ASC   'SEAU'
+            HEX   1F
+            ASC   'MANU'
+            HEX   20
+            ASC   'LIVR'
+            HEX   20
+            ASC   'ECHE'
+            HEX   21
+            ASC   'PASS'
+            HEX   22
+            ASC   'LAMP'
+            HEX   23
+            ASC   'TORC'
+            HEX   23
+            ASC   'BATT'
+            HEX   24
+            ASC   'FIOL'
+            HEX   25
+            ASC   'DELT'
+            HEX   26
+            ASC   'TOUR'
+            HEX   27
+            ASC   'AMPO'
+            HEX   28
+            ASC   'BOIT'
+            HEX   29
+            ASC   'CLEF'
+            HEX   2A
+            ASC   'MARM'
+            HEX   2B
+            ASC   'ANNE'
+            HEX   2C
+            ASC   'ROUG'
+            HEX   2D
+            ASC   'ORAN'
+            HEX   2E
+            ASC   'JAUN'
+            HEX   2F
+            ASC   'TRAP'
+            HEX   30
+            ASC   'SABL'
+            HEX   31
+            ASC   'ROBO'
+            HEX   32
+            ASC   'COLL'
+            HEX   35
+            ASC   'VISS'
+            HEX   36
+            ASC   'EAU '
+            HEX   37
+            ASC   'CASS'
+            HEX   39
+            ASC   'ESSA'
+            HEX   3A
+            ASC   'SENS'
+            HEX   3B
+            ASC   'RENI'
+            HEX   3B
+            ASC   'ADHE'
+            HEX   3D
+            ASC   'DECO'
+            HEX   3E
+            ASC   'PLAC'
+            HEX   3C
+            ASC   'RAME'
+            HEX   3F
+            ASC   'TUBE'
+            HEX   35
+            ASC   'LIST'
+            HEX   14
+            ASC   'BRAN'
+            HEX   33
+            ASC   'ESCA'
+            HEX   00
+            ASC   'SORS'
+            HEX   09
+            ASC   'AVAL'
+            HEX   19
+            ASC   'MAIL'
+            HEX   41
+            ASC   'MASQ'
+            HEX   42
+            ASC   'CHAU'
+            HEX   43
+            ASC   'ROBE'
+            HEX   44
+            ASC   'PORT'
+            HEX   45
+            ASC   'BROC'
+            HEX   46
+            ASC   'HARN'
+            HEX   47
+            ASC   'BOMB'
+            HEX   48
+            ASC   'METS'
+            HEX   5F
+            ASC   'ENFI'
+            HEX   5F
+            ASC   'PASS'
+            HEX   5F
+            ASC   'DONN'
+            HEX   4A
+            ASC   'VEND'
+            HEX   4C
+            ASC   'ACHE'
+            HEX   4D
+            ASC   'ENLE'
+            HEX   4E
+            ASC   'ARRA'
+            HEX   4E
+            ASC   'CREU'
+            HEX   4E
+            ASC   'PIER'
+            HEX   4F
+            ASC   'BLOC'
+            HEX   4F
+            ASC   'MUR '
+            HEX   4F
+            ASC   'ESPA'
+            HEX   50
+            ASC   'HACH'
+            HEX   51
+            ASC   'POT '
+            HEX   52
+            ASC   'LIAS'
+            HEX   53
+            ASC   'TABA'
+            HEX   54
+            ASC   'MEDE'
+            HEX   55
+            ASC   'CHAU'
+            HEX   56
+            ASC   'CREM'
+            HEX   57
+            ASC   'TRAI'
+            HEX   58
+            ASC   'BOUL'
+            HEX   59
+            ASC   'TAIL'
+            HEX   5A
+            ASC   'DROG'
+            HEX   5B
+            ASC   'TEMP'
+            HEX   5C
+            ASC   'DELI'
+            HEX   5D
+            ASC   'LIBE'
+            HEX   5D
+            ASC   'FEMM'
+            HEX   5E
+            ASC   'FILL'
+            HEX   5E
+            ASC   'DEBR'
+            HEX   60
+            ASC   'ASPI'
+            HEX   61
+            ASC   'QUIN'
+            HEX   62
+            ASC   'VETE'
+            HEX   4B
+            DB    $FF
+
+*
+* Les lieux
+*
+
+L52A0       ASC   00
+            ASC   'L'27'antre du demon.'00
+            ASC   'Le repere du chirurgien.'00
+            ASC   'X'00
+            ASC   'L'27'antre du sorcier.'00
+            ASC   'Au bord du lac.'00
+            ASC   'X'00
+            ASC   'Le repere du lecteur.'00
+            ASC   'Au nord,un banc de sable.'00
+            ASC   'Le bout du lac.'00
+            ASC   'La salle mecanique.'00
+            ASC   'Une grotte vide.'00
+            ASC   'La trappe des enrages.'00
+            ASC   'Une porte EXIT au sud.'00
+            ASC   'Une issue au sud.'00
+            ASC   'L'27'antre du lecteur.'00
+            ASC   'Des soupes colorees, dans des marmites.'00
+            ASC   'L'27'atelier.'00
+            ASC   'Une barque consolidee a l'27'adhesif..!'00
+            ASC   'La chambre des lumieres.'00
+            ASC   'Le refuge de l'27'alchimiste.'00
+            ASC   'Le gite du fakir.'00
+            ASC   'L'27'antre de la verite.'00
+            ASC   'L'27'antre du fou.'00
+            ASC   'L'27'antre du maigre.'00
+            ASC   'Le repere de l'27'embaumeur.'00
+            ASC   'Le gite du bricoleur.'00
+            ASC   'Le repere du fuyard.'00
+            ASC   'Le refuge du montagnard.'00
+            ASC   'Le chemin des rongeurs.'00
+            ASC   'L'27'antichambre de la mort.'00
+            ASC   'Du bruit a l'27'est.'00
+            ASC   'De la lumiere au sud.'00
+            ASC   'L'27'antre du maniaque.'00
+            ASC   'Le repere des rats.'00
+            ASC   'La salle des survivants.'00
+            ASC   'La cremerie et le tailleur.'00
+            ASC   'Le tabac et la boulangerie.'00
+            ASC   'Le traiteur.'00
+            ASC   'Le medecin et le chausseur.'00
+            ASC   'Le coin nord-ouest de la ville.'00
+            ASC   'La fin de la ville !'00
+            ASC   'Dans le tabac.'00
+            ASC   'X'00
+            ASC   'Chez le tailleur.'00
+            ASC   'Dans la cremerie.'00
+            ASC   'Chez le chausseur.'00
+            ASC   'Chez le medecin.'00
+            ASC   'La droguerie.'00
+            ASC   'Dans la droguerie.'00
+            ASC   'Le coin sud-est de la ville.'00
+            ASC   'Devant un monument..?'00
+            ASC   'Que vendez-vous ?'00
+            ASC   'La salle de la B.A.'00
+            ASC   'Le mausolee de l'27'exterminateur.'00
+            ASC   'Le choeur du Temple.'00
+            ASC   'L'27'antre du venere.'00
+            ASC   'Le chemin des dipteres.'00
+            ASC   'L'27'antre du victorieux.'00
+            ASC   00
+            ASC   00
+
+*
+* Les descriptions
+*
+
+L5760       ASC   00
+            ASC   'Vous etes arrive dans la salle de dis- '
+            ASC   ' section, et vous n'27'avez pas fait de   '
+            ASC   '  vieux os...'00
+            ASC   'Vous etes tombe dans l'27'incinerateur    '
+            ASC   ' d'27'organes ...'00
+            ASC   'Vous venez de faire une chute dans un  '
+            ASC   ' precipice d'27'au moins 200 m.'00
+            ASC   'Cette soupe etait un acide sulfurique  '
+            ASC   ' parfume... Hum! Douce mort.'00
+            ASC   'Il ne se passe rien, cela fait toujours'
+            ASC   ' un repas de gagne.'00
+            ASC   'La soupe est euphorisante, vous avez la'
+            ASC   ' vision d'27'une ville doree.'00
+            ASC   'Vous mourez etouffe dans les sables    '
+            ASC   ' mouvants...'00
+            ASC   'Vous trebuchez en l'27'abordant....       '
+            ASC   ' Miam, miam (les piranhas).'00
+            ASC   'Des gaz mortels s'27'echappent de la      '
+            ASC   ' bouteille... c'27'est triste.'00
+            ASC   'Un panneau du mur pivote, des COBRAS en'
+            ASC   ' sortent... Adieu !'00
+            ASC   'Vous trebuchez et vous vous empallez su'
+            ASC   'rle tournevis.'00
+            ASC   'Vous avez ete broye !!!'00
+            ASC   'Le robot vous broie les os...'00
+            ASC   'Un barreau de l'27'echelle se detache.'00
+            ASC   'Vous ne saviez pas piloter le DELTA    '
+            ASC   ' PLANE... Adieu !'00
+            ASC   'J'27'ai toujours pense qu'27'en 10 lecons le '
+            ASC   ' DELTA c'27'est juste.'00
+            ASC   'Vous avez ete happe par une presse     '
+            ASC   ' hydraulique...'00
+            ASC   'Vous grillez sur le generateur de la   '
+            ASC   ' salle d'27'operation.'00
+            ASC   'Vous glissez en montant, la colle gicle'
+            ASC   ' dans vos yeux.'00
+            ASC   'L'27'eau touche le systeme electrique...  '
+            ASC   ' Vous grillez...'00
+            ASC   'La folie vous a pris (vider un seau    '
+            ASC   ' vide), vous vous suicidez.'00
+            ASC   'Vous electrocutez le robot, mais vous  '
+            ASC   ' aussi par la meme occasion.'00
+            ASC   'Petit genie, hein ? Vous avez enraye le'
+            ASC   ' mecanisme du robot.'00
+            ASC   'Vous vous etes colles les doigts, et en'
+            ASC   ' voulant les separer a la lame de rasoi'
+            ASC   'r,vous vous etes tranche la gorge.'00
+            ASC   'Oh merci, merci beaucoup... SMACK !'00
+            ASC   'Vous l'27'avez deja fait.'00
+            ASC   'Il faudrait peut-etre debrancher la    '
+            ASC   ' batterie.'00
+            ASC   'Ce livre apprend le DELTA-PLANE en 10  '
+            ASC   ' lecons.'00
+            ASC   'L'27'endroit est un peu trop exigu pour   '
+            ASC   ' essayer une telle chose.'00
+            ASC   'Vous avez l'27'intention de vous shooter ?'00
+            ASC   'La boite explose !'00
+            ASC   'Il y a une clef sous l'27'adhesif.'00
+            ASC   'Cette fois vous en avez trop enleve, la'
+            ASC   ' barque part en miettes.'00
+            ASC   'La trappe est fermee a clef.'00
+            ASC   'Le passe partout ne fonctionne pas avec'
+            ASC   ' cette serrure.'00
+            ASC   'Les murs se rapprochent les uns des    '
+            ASC   ' autres, vous etes aplati.'00
+            ASC   'Si cela vous plait de perdre du temps !'00
+            ASC   'Il faudrait peut etre de l'27'electricite.'
+            ASC   '.'00
+            ASC   'Il faudrait peut etre y visser une     '
+            ASC   ' ampoule.'00
+            ASC   'IMPOSSIBLE'00
+            ASC   'Vous tombez dans un cercueil qui se    '
+            ASC   ' referme sur vous.'00
+            ASC   'Vous avez du attraper la rage aupres de'
+            ASC   ' rats...'00
+            ASC   'Vous avez mal digere la soupe, c'27'est   '
+            ASC   ' une intoxication.'00
+            ASC   'Vous avez ete electrocute par la lampe.'00
+            ASC   'A force de rester dans le noir, vous   '
+            ASC   ' etes devenu fou !'00
+            ASC   'Vous ne pouvez prendre, il faut acheter'
+            ASC   '.'00
+            ASC   'Le marchand est fou, il se jette sur   '
+            ASC   ' vous et vous tue.'00
+            ASC   'He! he! On ne rentre pas dans un temple'
+            ASC   ' avec ses chaussures, les gardes vous t'
+            ASC   'ue'00
+            ASC   'La piece etait pleine de mouches.......'
+            ASC   '.tse-tse !!!'00
+            ASC   'Les murs s'27'ecroulent sur vous, vous    '
+            ASC   ' n'27'auriez pas du la laisser, grand lach'
+            ASC   'e.'00
+            ASC   'Toutes mes felicitations.'00
+            ASC   'Il aurait fallu mettre le masque, les  '
+            ASC   ' gaz vous tue !'00
+            ASC   'La fille n'27'avait pas de masque, elle   '
+            ASC   ' meurt et vous aussi (le remord...).'00
+            ASC   'La fille etait nue, elle ne pouvait vou'
+            ASC   'ssuivre. Pour se venger elle vous tranc'
+            ASC   'hela gorge avec les debris.'00
+            ASC   'Vous n'27'avez rien pour trancher les     '
+            ASC   ' cordes.'00
+            ASC   'Vous auriez du eteindre la lampe avant '
+            ASC   ' de debrancher. Quel court circuit !!!'00
+            ASC   'Le possesseur du portefeuille devait   '
+            ASC   ' avoir la lepre...'00
+            ASC   'Vous trebuchez sur la hache...         '
+            ASC   ' Que de sang...'00
+            ASC   'Voici enfin la ville des mutiles.      '
+            ASC   ' Pauvres survivants des experiences    '
+            ASC   '  cruelles du savant fou.'00
+            ASC   'Vous avez du prendre mal par les pieds.'00
+            ASC   'La quinine etait trop forte, vous moure'
+            ASC   'zempoisonne...'00
+            ASC   'Je ne suis pas interesse.'00
+            ASC   'Alors, que faites vous la ?'
+            DB    $FF

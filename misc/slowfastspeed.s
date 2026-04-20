cd85559c26d6058eed560380859aa08a77ddb481
Antoine Vignau <34219772+antoinevignau@users.noreply.github.com>
2026-04-20 10:33:56 +0200

    for Jason

---
+++ misc/slowfastspeed.s
@@ -0,0 +1,82 @@
+*
+* Slow/Fast Speed
+*
+
+	mx	%11
+	lst	off
+
+	use	4/Misc.Macs
+	use	4/Util.Macs
+
+*----------------------------
+
+systemSpeed	=	$20	; index in BRAM (0: color, 1: mono)
+
+TRUE	=	1
+FALSE	=	0
+
+CYAREG	=	$c036
+IDROUTINE	=	$fe1f
+
+*----------------------------
+* Force slow speed
+
+setSLOWSPEED	sec		; are we on a IIgs?
+	jsr	IDROUTINE
+	bcc	IIgs	; yes
+	rts
+
+IIgs	clc		; full 16-bit
+	xce
+	rep	#$30
+
+	PushWord	#0	; get BRAM entry for speed
+	PushWord	#systemSpeed
+	_ReadBParam
+	pla
+	sta	bramSPEED	; save current value
+
+	PushWord	#FALSE	; we force slow speed
+	PushWord	#systemSpeed
+	_WriteBParam
+
+	sep	#$20
+	lda	CYAREG
+	sta	firmSPEED
+	and	#%0111_1111
+	sta	firmSPEED
+	
+exitNOW	sec
+	xce
+	sep	#$30
+	rts
+
+*----------------------------
+* Restore speed
+
+restoreSPEED	sec		; are we on a IIgs?
+	jsr	IDROUTINE
+	bcc	IIgs2	; yes
+	rts
+
+IIgs2	clc		; full 16-bit
+	xce
+	rep	#$30
+
+	PushWord	bramSPEED	; we restore the speed
+	PushWord	#systemSpeed
+	_WriteBParam
+
+	sep	#$20
+	lda	firmSPEED
+	sta	CYAREG
+	
+	sec
+	xce
+	sep	#$30
+	rts
+
+*----------------------------
+
+bramSPEED	ds	2
+firmSPEED	ds	1
